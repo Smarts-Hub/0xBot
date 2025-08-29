0111a8b4ec885b328de33adfeb87b351397e00c7
Jallox <javiluky2009@gmail.com>
2025-08-29 12:18:28 +0200

    organize restarter.js

--- core/modules/restarter.js
+++ core/modules/restarter.js
@@ -1,15 +1,13 @@
 import { spawn } from "child_process";
 
 export async function restart() {
-  const args = process.argv.slice(1); // todos los argumentos excepto "node"
-  const cmd = process.argv[0]; // normalmente "node"
+  const args = process.argv.slice(1); 
+  const cmd = process.argv[0]; 
 
-  // Lanzamos el mismo script
   const child = spawn(cmd, args, {
-    stdio: "inherit" // hereda la consola
+    stdio: "inherit" 
   });
 
-  // Terminamos el proceso actual
   child.on("close", (code) => {
     process.exit(code);
   });
