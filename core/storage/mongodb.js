adb04747185c90a00dbb0be2a253731016e8e330
Jallox <javiluky2009@gmail.com>
2025-09-01 22:51:31 +0200

    Update mongodb integration

--- core/storage/mongodb.js
+++ core/storage/mongodb.js
@@ -3,7 +3,7 @@ import config from '../../config/config.json' with { type: 'json' };
 import logger from "../logger.js";
 
 export async function connectMongoose() {
-    if(config.storage?.enabled !== true) return;
+    if(config.storage?.enabled != true) return;
     logger.info("Connecting to MongoDB...");
     await mongoose.connect(config.storage?.mongodb, {
         useNewUrlParser: true,
